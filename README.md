877bf6da94e7f9a99dfa96770084854fbf32f7d5
Ahmad <almohammedahmed2@gmail.com>
2020-11-01 21:26:08 +0200

    constructor had added

--- README.md
+++ README.md
@@ -9,13 +9,22 @@
 
 ## Actual time needed to complete: 59 mins
 
-
+----------------------------------------------------------------------------------------------------------------------
 
 ## Number and name of feature: 2
 
 ## Estimate of time needed to complete: 75 mins
 ## Start time: 2:50 pm
 
-## Finish time: 2:41pm
+## Finish time: 3:07pm
 
-## Actual time needed to complete: 59 mins
+## Actual time needed to complete: 17 mins
+----------------------------------------------------------------------------------------------------------------------
+## Number and name of feature: 3
+
+## Estimate of time needed to complete: 150 mins
+## Start time: 3:30 pm
+
+## Finish time: 3:07pm
+
+## Actual time needed to complete: 17 mins
--- server.js
+++ server.js
@@ -7,26 +7,63 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 app.use(cors());
-const superAgent = require('superagent');
+const superagent = require('superagent');
 const pg = require('pg');
 
-app.use(express.urlencoded({extended: true}));
-app.use('/public',express.static('./public'));
-app.set('view engine','ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static('./public'));
+app.set('view engine', 'ejs');
 // Reqired KEYS
 const PORT = process.env.PORT;
 
-app.get('/hello',indexRender);
+app.get('/hello', indexRender);
 
-function indexRender(req,res){
+function indexRender(req, res) {
     res.render('pages/index');
 };
 
-app.get('/',formRender);
+app.get('/', formRender);
+app.post('/searches', searchFunction);
 
-function formRender(req,res){
+function formRender(req, res) {
     res.render('pages/searches/new.ejs');
 }
+
+
+
+function Book(item) {
+    if(item.title){
+        
+    this.title = item.title;
+    }else{this.title = 'Not found'};
+
+    if(item.imageLinks.thumbnail){
+        this.image = item.imageLinks.thumbnail
+    }else{this.image = `https://i.imgur.com/J5LVHEL.jpg`};
+    
+    if(item.authors){
+        this.authors = item.authors;
+    }else{this.authors = 'Not found' }
+    if(item.description){
+        this.description = item.description;
+    }else{this.description ='Not found' }
+   
+}
+
+function searchFunction(req, res) {
+    
+    let q = '';
+    if (req.body.search[1] === 'title') { q = `+intitle:${req.body.search[0]}` }
+    if (req.body.search[1] === 'auther') { q = `+inauthor:${req.body.search[0]}` }
+
+    superagent.get(`https://www.googleapis.com/books/v1/volumes?q=${q}`)
+    
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => res.render('pages/searches/show', { searchResults: results }));
+    
+    
+}
+
 // server starting function
 app.listen(PORT, () => {
     console.log('app is listning on port' + PORT);
