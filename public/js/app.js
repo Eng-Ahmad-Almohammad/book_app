52f21598398cda8db357059483211d53efd77049
Ahmad <almohammedahmed2@gmail.com>
2020-11-03 19:47:46 +0200

    added delete and update button

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,16 @@
 'use strict';
-
-
-function menu() {
+​function menu() {
   if (document.getElementById('nav').style['display'] === 'none') {
     document.getElementById('nav').style['display'] = 'flex';
   } else if (document.getElementById('nav').style['display'] === 'flex') {
     document.getElementById('nav').style['display'] = 'none';
   }
-
+}
+​function update() {
+  event.preventDefault();
+  if (document.getElementById('updatearea').style['display'] === 'none') {
+    document.getElementById('updatearea').style['display'] = 'flex';
+  } else if (document.getElementById('updatearea').style['display'] === 'flex') {
+    document.getElementById('updatearea').style['display'] = 'none';
+  }
 }
